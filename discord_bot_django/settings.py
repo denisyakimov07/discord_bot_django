e167225952720060e1a6477b9d58ceefc67dc4e5
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2022-12-11 18:05:19 -0500

    init

--- discord_bot_django/settings.py
+++ discord_bot_django/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b28&*)z0*zmlmnew#*i09w(498g1j-ij4n#$b#a*uk8p3h7d$@'
+SECRET_KEY = get_env().SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
