a84e4df06fc0fe6ad9c2c93cd1ad913501c24725
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2022-12-11 19:40:43 -0500

    init

--- discord_bot_django/settings.py
+++ discord_bot_django/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = get_env().SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['150.136.153.131']
 
 
 # Application definition
